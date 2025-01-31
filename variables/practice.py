55c17ba1b9afdc25f6e2a20f2c8818a29723cd72
paige.jolley <paige.jolley@ucas-edu.net>
2025-01-31 11:17:29 -0700

    Variables Practice - Python

--- variables/practice.py
+++ variables/practice.py
@@ -1,7 +1,23 @@
 # Paige Jolley, Python Variable Practice
 
-name = "Paige"
+name = ("My name is Paige.")
 
-small_number = 2
+smallNumber = 4
 
 large_number = 101
+
+breakfast = ("I had a Waffle for breakfast.")
+
+color = ("My favorite color is Yellow.")
+
+school = ("The school name is UCAS.")
+
+year = 2025
+
+eye_color = ("My eye color is Hazel.")
+
+age = 14
+
+subject = ("My favorite subject Biology.")
+
+print(name, smallNumber, large_number, breakfast, color, school, year, eye_color, age, subject)
