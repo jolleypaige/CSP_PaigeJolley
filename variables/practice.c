2ace647071f9e2b8e0a7cf5eae69c58c93a08a18
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-03 09:01:22 -0700

    practice.c

---
+++ variables/practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char name[] = "Paige";
+int small_number = 3;
+int large_number = 102;
+char breakfast [] = "Waffle";
+char color [] = "Yellow";
+char school [] = "UCAS";
+char year [] = "Twenty-Twenty-Five";
+char age [] = "fourteen";
+char subject [] = "math";
+
+int main(void){
+    printf("My name is %s \n", name);
+    printf("A number betweem 1 and 10 is %d \n", small_number);
+    printf("A number between 100 and 1000 is %d \n", large_number);
+    printf("I had a %s for breakfast. \n", breakfast);
+    printf("My favorite color is %s. \n", color);
+    printf("The school name is %s. \n", school);
+    printf("The year is %s. \n", year);
+    printf("I am %s years old. \n", age);
+    printf("My favorite subject in school is %s. \n", subject)
+
+}
+  
