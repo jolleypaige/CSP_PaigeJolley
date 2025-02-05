8b37944edda9e13fe6bfd517bdc6251066fe0763
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-05 11:48:25 -0700

    expression_prac.c

---
+++ expressions/expressions_prac.c
@@ -0,0 +1,16 @@
+// Paige Jolley, TEMPLATE 
+#include <stdio.h>
+
+int equation = 7-24/8*4+6;
+int expression = 18/3-7+2*5;
+int problem = 6*4/12+72/8-9;
+int multi = (17-6/2)+4*3;
+
+
+int main(void){
+    printf("%d\n", equation);
+    printf("%d\n", expression);
+    printf("%d\n", problem);
+    printf("%d\n", multi);
+    return 0;
+}
