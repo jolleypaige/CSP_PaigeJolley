657e3d4ea2cf18c0da6966241cc528a70c1fda5d
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-06 11:15:23 -0700

    expression_prac.c

--- expressions/expressions_prac.c
+++ expressions/expressions_prac.c
@@ -5,12 +5,16 @@ int equation = 7-24/8*4+6;
 int expression = 18/3-7+2*5;
 int problem = 6*4/12+72/8-9;
 int multi = (17-6/2)+4*3;
-
+int math = -2*(1*4-2/2)+(6+2-3);
+int negative = -1*((3-4*7)/5)-2*24/6;
+int exponent = 
 
 int main(void){
     printf("%d\n", equation);
     printf("%d\n", expression);
     printf("%d\n", problem);
     printf("%d\n", multi);
+    printf("%d\n", math);
+    printf("%d\n", negative);
     return 0;
 }
