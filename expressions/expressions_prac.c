f52d89121c83384d6927161767bf907823de4c07
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-07 11:17:31 -0700

    expressions_prac.c

--- expressions/expressions_prac.c
+++ expressions/expressions_prac.c
@@ -1,19 +1,26 @@
 // Paige Jolley, Expression practice C 
 #include <stdio.h>
+#include <math.h>
 
-int equation = 7-24/8*4+6;
+int main(void){
+    int equation = 7-24/8*4+6;
 int expression = 18/3-7+2*5;
 int problem = 6*4/12+72/8-9;
 int multi = (17-6/2)+4*3;
 int math = -2*(1*4-2/2)+(6+2-3);
 int negative = -1*((3-4*7)/5)-2*24/6;
-
-int main(void){
+int exponent = (3*pow(5, 2)/15)-(5-pow(2, 2));
+int number = (pow(1, 4)*pow(2, 2)+pow(3, 3))-pow(2, 5)/4;
+int answer = (pow(22/2-2*5, 2))+(pow(4-6/6, 2));
     printf("%d\n", equation);
     printf("%d\n", expression);
     printf("%d\n", problem);
     printf("%d\n", multi);
     printf("%d\n", math);
     printf("%d\n", negative);
+    printf("%d\n", exponent);
+    printf("%d\n", number);
+    printf("%d\n", answer);
+
     return 0;
 }
