a605e2c8b9ea6cab15dce54b385bf9ecd226033a
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-10 15:44:39 -0700

    string notes

---
+++ strings/notes.py
@@ -0,0 +1,18 @@
+# Paige Jolley, String Notes Python
+
+# string is a data type that holds information surrounded by quotation marks "" ''
+
+#print("hello world")
+note = "Hello world"
+note = "Vienna's Class"
+
+# name =input("What is your first name? \n").strip().lower() # strip gets rid of spaces. Makes sure the letters are the same.
+
+# name =input("What is your first name? \n").capitalize() # makes sure they become not capitalized
+
+print(note)
+
+#print(f"Hello {name} welcome to my program!")
+
+sentance = "The quick brown fox jumps over the lazy dog"
+ 
