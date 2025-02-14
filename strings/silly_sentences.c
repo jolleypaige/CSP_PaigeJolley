622a82214d0588c4384d6236f92a1351c7b49c84
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-14 11:34:41 -0700

    silly_senteces.c

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -1,13 +1,28 @@
 // Paige Jolley, Silly Sentences C
 #include <stdio.h>
-// empty string variables for user words (minimum 3)
+#include <string.h>
 
 int main(void){
     // Welcome for the user telling them what the program is (print)
-
+    printf("Welcome! This program will create a silly story based on what the words write. \n");
+    
     // ask user for words (print statement with a questions scanf to set to variable) part of above (in C we need to tell the user that they can only typer 1 word)
+    char adjective[20];
+    printf("Name an adjective: ");
+    scanf("%s", adjective);
+
+    char noun[20];
+    printf("Name a noun: ");
+    scanf("%s", noun);
+
+    char verb[20];
+    printf("Name a verb: ");
+    scanf("%s", verb);
+
+
 
     //print out the story with the variables inserted ("welcome %s to my program", name)
+    printf("The %s pig %s the %s.", adjective, verb, noun);
 
 
     return 0;
