31bdfb4de9f3e2daa6535f0c9d44e6db255456e6
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-26 11:50:22 -0700

    financial_calc_update.c

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -2,72 +2,40 @@
 #include <stdio.h>
 #include <math.h>
 
-void info(cost, income, type){
-
-
-    
+float percent(float cost, float percentThing){
+    return((float)(cost/percentThing)*100);
 }
 
 
-void name(char bills[30]){
-    printf("Welcome! This program will be your financial calculator.\n");
+float name(char bills[]){
     printf("What is your %s?\n", bills);
-
 }
 
 int main(void){
-    bills("rent");
-    bills("rent");
-    bills("rent");
-    bills("rent");
-    bills("rent");
-    bills("rent");
-
-
-
-
-
-
-
-
-
-
-
-    int income;
-    printf("What is your income?\n");
-    scanf("%d", &income);
-
-    int rent;
-    printf("What is your rent?\n");
-    scanf("%d", &rent);
-
-    int utilities;
-    printf("What is the cost of your utilities?\n");
-    scanf("%d", &utilities);
-
-    int groceries;
-    printf("What is the cost of your groceries?\n");
-    scanf("%d", &groceries);
-
-    int transportation;
-    printf("What is the cost of your transportation?\n");
-    scanf("%d", &transportation);
+    printf("Welcome! This program will be your financial calculator.\n");
+    float income = name("income");
+    scanf("%f", &income);
+    float rent = name("rent");
+    scanf("%f", &rent);
+    float utilities = name("utilities");
+    scanf("%f", &utilities);
+    float groceries = name("groceries");
+    scanf("%f", &groceries);
+    float transportation = name("transportation");
+    scanf("%f", &transportation);
 
     float savings = income*.1;
 
-    float spending = (float)income-savings-rent-utilities-groceries-transportation;
+    float spendings = (float)income-savings-rent-utilities-groceries-transportation;
 
-    float per_rent = (float)rent/income *100;
 
-    float per_utilities = (float)utilities/income *100;
+    float per_rent = percent(rent, income);
+    float per_utilities = percent(utilities, income);
+    float per_groceries = percent(groceries, income);
+    float per_transportation = percent(transportation, income);
+    float per_spendings = percent(spendings, income);
 
-    float per_groceries = (float)groceries/income *100;
-
-    float per_transportation = (float)transportation/income *100; 
-
-    float per_spendings = (float)spending/income *100;
-
-    printf("Your rent is $%.2f and which is %.2f%% of your income.\n", rent, per_rent);
+     printf("Your rent is $%.2f and which is %.2f%% of your income.\n", rent, per_rent);
     
     printf("Your utilities is $%.2f and which is %.2f%% of your income.\n", utilities, per_utilities);
 
@@ -77,7 +45,6 @@ int main(void){
 
     printf("Your savings is $%.2f and which is 10%% of your income.\n", savings);
 
-    printf("Your spending is $%.2f and which is %.2f%% of your income.\n", spending, per_spendings);
+    printf("Your spending is $%.2f and which is %.2f%% of your income.\n", spendings, per_spendings);
     
-
 }
