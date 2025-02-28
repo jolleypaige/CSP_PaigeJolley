7d7cfb7c8d4a6a328da61f459aa255081a6046ac
paige.jolley <paige.jolley@ucas-edu.net>
2025-02-28 11:32:50 -0700

    old_enough.c

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -8,10 +8,10 @@ scanf("%d", &age);
      
 if(age == 15){
     printf("You can get a learners permit.\n");
-}else if(age == 16){
-    printf("You can drive.\n");
 }else if(age >= 18){
     printf("You can vote.\n");
+}else if(age >= 16){
+    printf("You can drive.\n");
 }else if(age >= 6){
     printf("You can go to school\n");
 }else{
