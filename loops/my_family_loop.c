ba656a255799fd9a71fe8793da8213963e3e1351
paige.jolley <paige.jolley@ucas-edu.net>
2025-03-07 11:15:58 -0700

    my_family_loops.c

---
+++ loops/my_family_loop.c
@@ -0,0 +1,12 @@
+// Paige Jolley, My family loop, Python
+#include <stdio.h>
+
+int main(void){
+char family[][20] = {"Curtis","Mark","Eric","Amy","Creg"};
+    printf("Hello %s\n", family[0]);
+    printf("Hello %s\n", family[1]);
+    printf("Hello %s\n", family[2]);
+    printf("Hello %s\n", family[3]);
+    printf("Hello %s\n", family[4]);
+    return 0;  
+}
