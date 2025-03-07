978217adf6681e24c8a7a061f06bf26823c26684
paige.jolley <paige.jolley@ucas-edu.net>
2025-03-07 11:18:30 -0700

    my_family_loop.py

--- loops/my_family_loop.py
+++ loops/my_family_loop.py
@@ -1,8 +1,5 @@
 # Paige Jolley, My family loop Python
 
 siblings = ["Curtis","Mark","Eric","Amy","Creg"]
-print("Hello!", siblings[0])
-print("Hello!", siblings[1])
-print("Hello!", siblings[2])
-print("Hello!", siblings[3])
-print("Hello!", siblings[4])
+for siblings in siblings:
+        print("Hello", siblings)
