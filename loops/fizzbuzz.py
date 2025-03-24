2ba4f460d86d07ffd7cb35db568aa922ff650bdb
jolleypaige <paige.jolley@ucas-edu.net>
2025-03-24 11:24:43 -0600

    fizzbuzz.py

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,1 +1,11 @@
 # Paige Jolley, FizzBuzz, Python
+
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizzbuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
