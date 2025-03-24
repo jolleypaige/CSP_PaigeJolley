0bbff2260ec4e1fa3430841ad1aa98c084164556
jolleypaige <paige.jolley@ucas-edu.net>
2025-03-24 11:19:25 -0600

    fizzbuzz.c

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,7 +1,18 @@
 // Paige Jolley, FizzBuzz C
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
-    printf("Hello World");
+    for(int i =1; i <= 50; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            printf("FizzBuzz\n");
+        } else if(i % 3 == 0){
+            printf("Fizz\n");
+        } else if(i % 5 == 0){
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }   
     return 0;  
 }
