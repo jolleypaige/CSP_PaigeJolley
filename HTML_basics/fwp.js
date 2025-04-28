2e5d3718125bd0e1a44a24723638bd87b0c0edba
jolleypaige <paige.jolley@ucas-edu.net>
2025-04-28 11:41:04 -0600

    first_web_page.html

--- HTML_basics/fwp.js
+++ HTML_basics/fwp.js
@@ -1,6 +1,18 @@
-function show(){
-    document.getElementById("lost").style.display = "block"
+let images = ["https://www.un.org/sites/un2.un.org/files/2021/08/feeling-the-heat.jpg", "https://www.un.org/sites/un2.un.org/files/2021/08/earth-asking-for-help.jpg"]
+let counter = 0
+
+function change(){
+    if(counter < images.length){
+        document.getElementById("img"). 
+    src = images[counter]
+    counter += 1
+}else{
+    counter = 0
+    document.getElementById("img"). 
+    src = images[counter]
 }
+}
+
 function view(){
     if(document.getElementById("more").style.display === "block"){
         document.getElementById("more").style.display = "none"
