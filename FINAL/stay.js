3bb806c14a5590358ef88d30c2ac65c9379da9c4
jolleypaige <paige.jolley@ucas-edu.net>
2025-05-01 13:27:13 -0600

    staycation.html

--- FINAL/stay.js
+++ FINAL/stay.js
@@ -1,4 +1,4 @@
-let images = ["https://www.travelmanagers.com.au/wp-content/uploads/2013/02/AdobeStock_113476152_Bonneville-Salt-Flats-Utah-USA_750x375.jpg", "https://i0.wp.com/www.culturalchromatics.com/wp-content/uploads/2016/02/BonnevilleSaltFlats-%C2%A9NanetteWong-7.jpg?resize=660%2C440&ssl=1"]
+let images = ["https://i0.wp.com/www.culturalchromatics.com/wp-content/uploads/2016/02/BonnevilleSaltFlats-%C2%A9NanetteWong-3.jpg?resize=660%2C440&ssl=1", "https://i0.wp.com/www.culturalchromatics.com/wp-content/uploads/2016/02/BonnevilleSaltFlats-%C2%A9NanetteWong-7.jpg?resize=660%2C440&ssl=1"]
 let counter = 0
 function change(){
     if(counter < images.length){
@@ -10,4 +10,14 @@ function change(){
     document.getElementById("img"). 
     src = images[counter]
 }
+}
+function view(){
+    if(document.getElementById("more").style.display === "block"){
+        document.getElementById("more").style.display = "none"
+        document.getElementById("show").innerHTML = "Want to Learn About the Canals? Click Here to Show More!"
+    }else{
+        document.getElementById("more").style.display = "block"
+        document.getElementById("show").innerHTML = "I'm Done Learning... Show Less"
+    }
+ 
 }
